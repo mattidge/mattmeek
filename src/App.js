3f27dd2960e01e364d46c0b370acf85592a84496
Matthew Meek <Matt@matthews-mbp.lan>
2023-09-02 12:41:17 +0100

    friday app.js file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import ClickableImage from './components/clickableimage/ClickableImage';
+import linkedin from "./assets/images/linkedin.png";
+import facebook from "./assets/images/facebook.png";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <ClickableImage image={linkedin} url={"https://www.linkedin.com/in/matthew-meek-564797160"} />
+        <ClickableImage image={facebook} url={"https://www.facebook.com/matthew.l.meek/"}/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
