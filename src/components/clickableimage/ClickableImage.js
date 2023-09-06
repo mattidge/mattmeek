32033def311fa9cdfc380ff5704417e2811df7e6
Matthew Meek <Matt@matthews-mbp.lan>
2023-09-06 21:49:49 +0100

    before I start doing shit

--- src/components/clickableimage/ClickableImage.js
+++ src/components/clickableimage/ClickableImage.js
@@ -1,9 +1,10 @@
-import "./ClickableImage.css";
+import React from 'react';
+import './ClickableImage.css';
 
 function ClickableImage(props) {
   return (
     <a href={props.url} target="_blank">
-        <img src={props.image} className="image"/>
+      <img src={props.image} className="image" alt="Clickable Image" />
     </a>
   );
 }
