39b942adeeb088aac557b0b5c8cc3cadcacb4129
Matthew Meek <Matt@matthews-mbp.lan>
2023-09-06 20:46:42 +0100

    removing the react stuff

--- src/App.js
+++ src/App.js
@@ -9,20 +9,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <ClickableImage image={linkedin} url={"https://www.linkedin.com/in/matthew-meek-564797160"} />
         <ClickableImage image={facebook} url={"https://www.facebook.com/matthew.l.meek/"}/>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
